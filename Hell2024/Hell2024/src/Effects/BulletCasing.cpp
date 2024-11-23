3c33f1d8135abe2ed7095befe590ae978a00f8fa
FragStudios <jaydenxwragg@outlook.com>
2024-11-23 02:19:44 +0000

    qqqqq

--- Hell2024/Hell2024/src/Effects/BulletCasing.cpp
+++ Hell2024/Hell2024/src/Effects/BulletCasing.cpp
@@ -1,23 +1,30 @@
 #include "BulletCasing.h"
+
 #include "../Util.hpp"
 #include "../Core/Audio.hpp"
 #include "../Physics/Physics.h"
 
-#define BULLET_CASING_LIFETIME 2.0f
-
-void BulletCasing::CleanUp() {
+// Clean up the bullet casing
+void BulletCasing::CleanUp() 
+{
     Physics::Destroy(m_pxShape);
     Physics::Destroy(m_pxRigidBody);
 }
 
-glm::mat4 BulletCasing::GetModelMatrix() {
+// A getter for the 4x4 matrix used for the bullet casings position, rotation and scale relative to the world
+glm::mat4 BulletCasing::GetModelMatrix() 
+{
     return m_modelMatrix;
 }
 
-void BulletCasing::Update(float deltaTime) {
+// Update the bullet casing time, collision group, scale and cleanup
+void BulletCasing::Update(float deltaTime) 
+{
     m_lifeTime += deltaTime;
+
     // These don't have collision right away because it fucks with the muzzle flash
-    if (m_pxRigidBody && !m_collisionsEnabled && m_lifeTime > 0.0005) {
+    if (m_pxRigidBody && !m_collisionsEnabled && m_lifeTime > 0.0005) 
+    {
         PxShape* shape;
         m_pxRigidBody->getShapes(&shape, 1);
         PxFilterData filterData = shape->getQueryFilterData();
@@ -26,19 +33,26 @@ void BulletCasing::Update(float deltaTime) {
         shape->setSimulationFilterData(filterData);
         m_collisionsEnabled = true;
     }
-    if (m_lifeTime < BULLET_CASING_LIFETIME) {
+
+    if (m_lifeTime < BULLET_CASING_LIFETIME) 
+    {
         Transform localTransform;
         localTransform.scale *= glm::vec3(2.0f);
         m_modelMatrix = Util::PxMat44ToGlmMat4(m_pxRigidBody->getGlobalPose()) * localTransform.to_mat4();
     }
-    else if (m_pxRigidBody) {
+    else if (m_pxRigidBody) 
+    {
         CleanUp();
     }
 }
 
-void BulletCasing::CollisionResponse() {
-    if (m_audioDelay == 0) {
-        Audio::PlayAudio("BulletCasingBounce.wav", Util::RandomFloat(0.1f, 0.2f));
+// When we hit a collision with the bullet, play a sound
+void BulletCasing::CollisionResponse() 
+{
+    if (m_audioDelay == 0) 
+    {
+        Audio::PlayAudio("BulletCasingBounce.wav", Util::RandomFloat(0.1f, 0.6f));
     }
-    m_audioDelay = 0.1f;
+
+    m_audioDelay = 0.2f;
 }
--- Hell2024/Hell2024/src/Effects/BulletCasing.h
+++ Hell2024/Hell2024/src/Effects/BulletCasing.h
@@ -2,12 +2,17 @@
 #include "HellCommon.h"
 #include "../Physics/Physics.h"
 
+constexpr auto static BULLET_CASING_LIFETIME = 2.0f;
+
 struct BulletCasing {
     PxShape* m_pxShape = NULL;
     PxRigidBody* m_pxRigidBody = NULL;
+
     float m_audioDelay = 0.0f;
     float m_lifeTime = 0.0f;
+
     glm::mat4 m_modelMatrix = glm::mat4(1);
+
     bool m_collisionsEnabled = false;
     int m_modelIndex;
     int m_materialIndex;
@@ -15,5 +20,6 @@ struct BulletCasing {
     void Update(float deltaTime);
     void CollisionResponse();
     void CleanUp();
+
     glm::mat4 GetModelMatrix();
 };
